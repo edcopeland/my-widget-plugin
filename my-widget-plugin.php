4ad34e16137b970e7f37b686acd48faba0e665a0
Ed Copeland <ed_copeland@hotmail.co.uk>
2018-01-20 19:32:19 +0000

    add radio buttons

--- my-widget-plugin.php
+++ my-widget-plugin.php
@@ -11,7 +11,6 @@ License: GPL2
 
 // The widget class
 class My_Custom_Widget extends WP_Widget {
-
 	// Main constructor
 	public function __construct() {
 		parent::__construct(
@@ -22,10 +21,8 @@ class My_Custom_Widget extends WP_Widget {
 			)
 		);
 	}
-
 	// The widget form (for the backend )
 	public function form( $instance ) {
-
 		// Set widget defaults
 		$defaults = array(
 			'title'    => '',
@@ -33,8 +30,9 @@ class My_Custom_Widget extends WP_Widget {
 			'textarea' => '',
 			'checkbox' => '',
 			'select'   => '',
+      'radio_btn'    => '',
 		);
-		
+
 		// Parse current settings with defaults
 		extract( wp_parse_args( ( array ) $instance, $defaults ) ); ?>
 
@@ -74,17 +72,36 @@ class My_Custom_Widget extends WP_Widget {
 				'option_2' => __( 'Option 2', 'text_domain' ),
 				'option_3' => __( 'Option 3', 'text_domain' ),
 			);
-
 			// Loop through options and add each one to the select dropdown
 			foreach ( $options as $key => $name ) {
 				echo '<option value="' . esc_attr( $key ) . '" id="' . esc_attr( $key ) . '" '. selected( $select, $key, false ) . '>'. $name . '</option>';
-
 			} ?>
 			</select>
 		</p>
 
-	<?php }
+    <p>
 
+			<?php
+			$radio_options = array(
+				'option_a' => 'option a',
+				'option_b' => 'option b',
+				'option_c' => 'option c',
+			);
+			foreach( $radio_options as $radio_button ) {
+
+					$checked = ( isset( $instance['radio_btn'] ) && $radio_button === $instance['radio_btn'] ) ? ' checked="checked"' : '' ;
+					printf( '<label> <input class="radio" id="%1$s" name="%2$s" type="radio" value="%3$s" %4$s /> <span > %5$s </span> </label>' . "\n",
+						$this->get_field_id( 'radio_btn' ),   // id
+						$this->get_field_name( 'radio_btn' ), // name
+						esc_attr( $radio_button ),         		// value
+						$checked,                          		// checked
+						esc_html($radio_button)
+					);
+				}
+				?>
+    </p>
+	<?php
+} //end form
 	// Update widget settings
 	public function update( $new_instance, $old_instance ) {
 		$instance = $old_instance;
@@ -93,61 +110,51 @@ class My_Custom_Widget extends WP_Widget {
 		$instance['textarea'] = isset( $new_instance['textarea'] ) ? wp_kses_post( $new_instance['textarea'] ) : '';
 		$instance['checkbox'] = isset( $new_instance['checkbox'] ) ? 1 : false;
 		$instance['select']   = isset( $new_instance['select'] ) ? wp_strip_all_tags( $new_instance['select'] ) : '';
+    $instance['radio_btn']    = isset( $new_instance['radio_btn']) ? wp_strip_all_tags( $new_instance['radio_btn']) : '';
 		return $instance;
 	}
-
 	// Display the widget
 	public function widget( $args, $instance ) {
-
 		extract( $args );
-
 		// Check the widget options
 		$title    = isset( $instance['title'] ) ? apply_filters( 'widget_title', $instance['title'] ) : '';
 		$text     = isset( $instance['text'] ) ? $instance['text'] : '';
 		$textarea = isset( $instance['textarea'] ) ?$instance['textarea'] : '';
 		$select   = isset( $instance['select'] ) ? $instance['select'] : '';
 		$checkbox = ! empty( $instance['checkbox'] ) ? $instance['checkbox'] : false;
-
+    $radio    = isset( $instance['radio_btn' ] ) ? $instance['radio_btn'] : '';
 		// WordPress core before_widget hook (always include )
 		echo $before_widget;
-
 		// Display the widget
 		echo '<div class="widget-text wp_widget_plugin_box">';
-
 			// Display widget title if defined
 			if ( $title ) {
 				echo $before_title . $title . $after_title;
 			}
-
 			// Display text field
 			if ( $text ) {
 				echo '<p>' . $text . '</p>';
 			}
-
 			// Display textarea field
 			if ( $textarea ) {
 				echo '<p>' . $textarea . '</p>';
 			}
-
 			// Display select field
 			if ( $select ) {
 				echo '<p>' . $select . '</p>';
 			}
-
 			// Display something if checkbox is true
 			if ( $checkbox ) {
 				echo '<p>Something awesome</p>';
 			}
-
+      if ( $radio ) {
+        echo '<p>' . $radio . '</p>';
+      }
 		echo '</div>';
-
 		// WordPress core after_widget hook (always include )
 		echo $after_widget;
-
 	}
-
 }
-
 // Register the widget
 function my_register_custom_widget() {
 	register_widget( 'My_Custom_Widget' );
